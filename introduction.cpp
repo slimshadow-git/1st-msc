80f513372df82882220f641a1264f0071f4f10ef
slimshadow-git <130247009+slimshadow-git@users.noreply.github.com>
2024-09-02 12:57:22 +0530

    Create introduction.cpp

---
+++ introduction.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // This is a header file that includes the standard input/output stream objects
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // This line prints "Hello, World!" to the console
+    return 0;  // The program ends here, returning 0 indicates successful execution
+}
